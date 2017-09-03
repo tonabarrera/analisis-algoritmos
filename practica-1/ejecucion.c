00c1e5fb5aa04fdb459c932daee432f1b8e1ca43
Tonatihu <carlostonatihu@gmail.com>
2017-09-03 13:44:05 -0500

    Nada

--- practica-1/ejecucion.c
+++ practica-1/ejecucion.c
@@ -10,7 +10,7 @@ int main(int size, char**args){ // los argumentos son, archivo n_elementos
     if(archivo == NULL){return error(ARCHIVO_NULO, args[1]);}
 
     llenar_arreglo(archivo, &numeros, n, args[1]);
-    ordenamiento_burbuja(&numeros, n);
+    ordenamiento_arbol(&numeros, n);
     imprimir_arreglo(numeros, n);
 
     return 0;
